a1c54587121c6c486a9f45829287a65076827d7b
Raghav <54115719+raghav8802@users.noreply.github.com>
2024-08-23 00:54:45 +0530

    Update page.tsx

---
+++ src/app/(auth)/forgotpassword/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ForgotPasswordPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Forgot Password</h1>
+      <p>Please enter your email to reset your password.</p>
+      {/* Add your form or additional content here */}
+    </div>
+  );
+};
+
+export default ForgotPasswordPage;
