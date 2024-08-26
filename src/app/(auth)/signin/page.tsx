a0a59eef2b6ccce888a04db722cd5e98d6d3a8a9
Mrpradhanji <114905691+Mrpradhanji@users.noreply.github.com>
2024-08-26 22:16:51 +0530

    Testing

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -65,7 +65,7 @@ const signIn = () => {
   }
 
   return (
-    // <div className=c>
+  
     <div className={`flex items-center justify-center w-full h-screen ${Styles.mainContainer}`}>
       <div className={` ${Styles.containerLeft}`}>
         <div className={Styles.containerLeftInner}>
